cb918e91ea679f6cc3deca5673cd522ba289b2a6
Alice-Githui <gnyokabi.alice@gmail.com>
2021-06-14 22:40:01 +0300

    Refactor the portfolio images and links to projects

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,18 @@
-const images=document.querySelectorAll(".project-photos img");
-const modal=document.getElementById('myModal');
-const modalImg=document.querySelector(".modalImg");
-const modalTxt=document.querySelector(".modalTxt");
-const close=document.querySelector(".close");
+// const images=document.querySelectorAll(".project-photos img");
+// const modal=document.getElementById('myModal');
+// const modalImg=document.querySelector(".modalImg");
+// const modalTxt=document.querySelector(".modalTxt");
+// const close=document.querySelector(".close");
+// const viewproject=document.querySelector(".viewproject")
 
-images.forEach((image)=>{
-    image.addEventListener("click", ()=>{
-        modalImg.src=image.src;
-        modalTxt.innerHTML=image.alt;
-        modal.style.display="block"
+// images.forEach((image)=>{
+//     image.addEventListener("click", ()=>{
+//         modalImg.src=image.src;
+//         modal.style.display="block"
 
-        close.addEventListener("click", ()=>{
-            modal.style.display="none"
-    })
+//         close.addEventListener("click", ()=>{
+//             modal.style.display="none"
+//     })
 
-    })
-})
+//     })
+// })
